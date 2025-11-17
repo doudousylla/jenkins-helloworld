31b22be6682b391c4fcafc615695bd0a1cc80035
Mahamadou Sylla <mahamadousylla167@gmail.com>
2025-11-17 22:43:18 +0100

    Update greeting message in Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Mahamadou SYLLA!");
+        System.out.println("Hello, Mahamadou SYLLA! test webhook");
     }
 }
